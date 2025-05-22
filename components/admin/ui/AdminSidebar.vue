3019df1e148390be2bfb5996882ff52dab54f27e
elodie <elodiemalet.web@gmail.com>
2025-05-22 18:21:59 +0200

    (fix) logout

--- components/admin/ui/AdminSidebar.vue
+++ components/admin/ui/AdminSidebar.vue
@@ -167,7 +167,7 @@
                                 leave-from-class="transform opacity-100 scale-100"
                                 leave-to-class="transform opacity-0 scale-95">
                                 <MenuItems
-                                    class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                                    class="absolute right-0 z-10 mt-2.5 w-40 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                                     <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
                                         <a
                                             :href="item.href"
@@ -273,9 +273,10 @@ export default {
     },
     methods: {
         userLogout() {
-            const {fetch: logout} = useUserSession();
-            logout();
-            navigateTo('/');
+            const {clear} = useUserSession();
+            clear().then(() => {
+                navigateTo('/login');
+            });
         }
     },
 };
