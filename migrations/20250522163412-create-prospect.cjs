2affeb076869eab9d537491793d80d508a6e5d60
elodie <elodiemalet.web@gmail.com>
2025-05-22 18:43:36 +0200

    (feat) prospects table

---
+++ migrations/20250522163412-create-prospect.cjs
@@ -0,0 +1,30 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Prospects', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface) {
+        await queryInterface.dropTable('Prospects');
+    }
+};
