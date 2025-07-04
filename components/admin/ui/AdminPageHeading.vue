3ea50f1fc75b07362682c380bbb3bc8f0b034c5e
elodie <elodiemalet.web@gmail.com>
2025-07-04 13:53:59 +0200

    (feat) remove breadcrumb

--- components/admin/ui/AdminPageHeading.vue
+++ components/admin/ui/AdminPageHeading.vue
@@ -13,43 +13,43 @@
                     Back
                 </a>
             </nav>
-            <nav
-                class="hidden sm:flex"
-                aria-label="Breadcrumb">
-                <ol
-                    role="list"
-                    class="flex items-center space-x-4">
-                    <li>
-                        <div class="flex">
-                            <a
-                                href="#"
-                                class="text-sm font-medium text-gray-500 hover:text-gray-700">Jobs</a>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="flex items-center">
-                            <ChevronRightIcon
-                                class="size-5 shrink-0 text-gray-400"
-                                aria-hidden="true"/>
-                            <a
-                                href="#"
-                                class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Engineering</a>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="flex items-center">
-                            <ChevronRightIcon
-                                class="size-5 shrink-0 text-gray-400"
-                                aria-hidden="true"/>
-                            <a
-                                href="#"
-                                aria-current="page"
-                                class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Back End
-                                Developer</a>
-                        </div>
-                    </li>
-                </ol>
-            </nav>
+            <!--            <nav-->
+            <!--                class="hidden sm:flex"-->
+            <!--                aria-label="Breadcrumb">-->
+            <!--                <ol-->
+            <!--                    role="list"-->
+            <!--                    class="flex items-center space-x-4">-->
+            <!--                    <li>-->
+            <!--                        <div class="flex">-->
+            <!--                            <a-->
+            <!--                                href="#"-->
+            <!--                                class="text-sm font-medium text-gray-500 hover:text-gray-700">Jobs</a>-->
+            <!--                        </div>-->
+            <!--                    </li>-->
+            <!--                    <li>-->
+            <!--                        <div class="flex items-center">-->
+            <!--                            <ChevronRightIcon-->
+            <!--                                class="size-5 shrink-0 text-gray-400"-->
+            <!--                                aria-hidden="true"/>-->
+            <!--                            <a-->
+            <!--                                href="#"-->
+            <!--                                class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Engineering</a>-->
+            <!--                        </div>-->
+            <!--                    </li>-->
+            <!--                    <li>-->
+            <!--                        <div class="flex items-center">-->
+            <!--                            <ChevronRightIcon-->
+            <!--                                class="size-5 shrink-0 text-gray-400"-->
+            <!--                                aria-hidden="true"/>-->
+            <!--                            <a-->
+            <!--                                href="#"-->
+            <!--                                aria-current="page"-->
+            <!--                                class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Back End-->
+            <!--                                Developer</a>-->
+            <!--                        </div>-->
+            <!--                    </li>-->
+            <!--                </ol>-->
+            <!--            </nav>-->
         </div>
         <div class="mt-2 md:flex md:items-center md:justify-between">
             <div class="min-w-0 flex-1">
